4b717d95ea75efa34d3ad5ea9e2b345189105aeb
limzm94 <75262986+limzm94@users.noreply.github.com>
2023-09-04 15:41:07 +0800

    Initial
    Initial push

---
+++ src/Encoder.java
@@ -0,0 +1,9 @@
+public class Encoder {
+    int offset;
+    String message;
+
+    char[] reference = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z', ''}
+
+}
